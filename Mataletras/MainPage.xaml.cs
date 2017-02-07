d4787289a55a2f3cb213f4c7d25e9b8084298c00
forameus <navarrogordilloalberto@gmail.com>
2017-02-07 13:00:52 +0100

    Hecho prototipo

---
+++ Mataletras/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Mataletras
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Random random;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            CoreWindow.GetForCurrentThread().KeyUp += Grid_KeyUp;
+            random = new Random();
+            SigLetra();
+        }
+
+        public void SigLetra()
+        {
+            int num = random.Next(0, 26);
+            char let = (char)('A' + num);
+            Texto.Text = let.ToString();
+        }
+
+        private void Grid_KeyUp(CoreWindow sender, KeyEventArgs e)
+        {           
+            if (e.VirtualKey.ToString() == Texto.Text)
+                SigLetra();
+        }
+    }
+}

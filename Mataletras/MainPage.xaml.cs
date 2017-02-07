d3f48c91ed5feb35acbafc6ac5f8a8d29d8a2862
forameus <navarrogordilloalberto@gmail.com>
2017-02-07 14:35:57 +0100

    Hecho prototipo con palabras

--- Mataletras/MainPage.xaml.cs
+++ Mataletras/MainPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Mataletras.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Core;
@@ -14,8 +16,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace Mataletras
 {
     /// <summary>
@@ -24,15 +24,37 @@ namespace Mataletras
     public sealed partial class MainPage : Page
     {
         private Random random;
+        private Palabra[] palabras;
+        private Palabra palabraActual;        
 
         public MainPage()
         {
             this.InitializeComponent();
-            CoreWindow.GetForCurrentThread().KeyUp += Grid_KeyUp;
+            CoreWindow.GetForCurrentThread().KeyUp += pulsarTecla;
             random = new Random();
-            SigLetra();
+            palabras = new Palabra[3] { new Palabra("PELOTA"), new Palabra("CASA"), new Palabra("INTERNET") };
+            SigPalabra();
         }
 
+        
+
+        private void pulsarTecla(CoreWindow sender, KeyEventArgs e)
+        {
+            if (palabraActual.quitarLetra(Char.ToUpper(e.VirtualKey.ToString()[0])))
+            {
+                if (palabraActual.letras.Length==0)
+                    SigPalabra();                
+                else
+                    Texto.Text = palabraActual.letras;
+            }                
+            /*if (e.VirtualKey.ToString() == Texto.Text)
+                SigLetra();*/
+        }
+
+
+        /// <summary>
+        /// Escribe una letra aleatoria en el TextBlock de la MainPage
+        /// </summary>
         public void SigLetra()
         {
             int num = random.Next(0, 26);
@@ -40,10 +62,15 @@ namespace Mataletras
             Texto.Text = let.ToString();
         }
 
-        private void Grid_KeyUp(CoreWindow sender, KeyEventArgs e)
-        {           
-            if (e.VirtualKey.ToString() == Texto.Text)
-                SigLetra();
+
+        /// <summary>
+        /// Escribe una palabra aleatoria en el TextBlock de la MainPage
+        /// </summary>
+        public void SigPalabra()
+        {
+            palabraActual = new Palabra(palabras[random.Next(0, palabras.Length)].letras);
+            Debug.Write(palabraActual.letras);
+            Texto.Text = palabraActual.letras;
         }
-    }
+    }   
 }

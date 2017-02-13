f4ef94cccf7581e447508763b53dd4f19ada4b3d
zoren <dark.zoren@gmail.com>
2017-02-13 22:56:49 +0100

    Añadida navecita y fondo
    La navecita está hecha por mi (zoren15) ;)

--- Mataletras/Models/Palabra.cs
+++ Mataletras/Models/Palabra.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 
 namespace Mataletras.Models
@@ -22,6 +24,7 @@ namespace Mataletras.Models
         {
             this.letras = letras;
             textBlock = new TextBlock() { Text = letras };
+            textBlock.Foreground = new SolidColorBrush(Colors.White);
         }
 
         /// <summary>
